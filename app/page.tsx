99e1d49f4e0f4a96a6ea16f17c7689b39f366e1d
Sandesh Pulami <sandeshpulami333@gmail.com>
2025-10-20 21:02:30 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+
+export default function Home(){
+  return(
+    <div>
+      <h1>
+        Get Started
+      </h1>
+      <Button>Start</Button>
+    </div>
+  );
+}
